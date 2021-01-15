7b0513f031af68fa677de77c0c016f44c361abdf
MaxKonovalov38 <max.kon.38r@gmail.com>
2021-01-15 16:29:49 +0800

    mod func simple_out: sum lenthg fileline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn help_out() {
 	// Полная справка о программе
@@ -6,22 +9,45 @@ fn help_out() {
 	text_hp.push_str(env!("CARGO_PKG_VERSION"));
 	text_hp.push_str("), ");
 	text_hp.push_str(env!("CARGO_PKG_DESCRIPTION"));
-	println!("{}", text_hp);
+	println!("{}\n##EXAMPLE:\n\tcargo run [--] [arg] [arg] ...", text_hp);
 }
+
 fn title_out() {
 	// Краткое пояснение работы
 	println!("My program WC!\nEXAMPLE: cargo run [--] [arg] [arg] ...");
 }
+
 fn long_out(_par: &str, _file: &str) {
 	// Выполнение с доп. параметрами
 	println!("Parametr: {}, File: {}", _par, _file);
 }
-fn simple_out(_file: &str) {
+
+fn simple_out(_file_name: &str) {
 	// Стандартное выполнение
-	if _file == "-h" {
+	if _file_name == "-h" {
 		help_out();
 	} else {
-		println!("File: {}", _file);
+		println!("[ INFO ] Starting calc!");
+
+		// переменная с типом Path
+		let input_file = Path::new(_file_name); 
+
+		// переменная для открытия файла
+		let file = File::open(&input_file)
+			.expect("[ ERROR ] Failed to open file!");
+
+		// переменная содержит полное содержимое file
+		let reader = BufReader::new(file);
+
+		let mut sum = 0;
+
+		//
+		for line in reader.lines() {
+			let line_cont = line.unwrap();
+			sum += line_cont.len();
+		}
+
+		println!("Word count: {}", sum);
 	}
 }
 
