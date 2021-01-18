52cd52a77b993bca356e03b6373d87e1dc9de959
MaxKonovalov38 <max.kon.38r@gmail.com>
2021-01-18 20:04:30 +0800

    mod func's title, help, long

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
-use std::fs::File;
 use std::io::{BufRead, BufReader};
+
+mod lib;
 
 fn help_out() {
 	// Полная справка о программе
@@ -9,45 +9,61 @@ fn help_out() {
 	text_hp.push_str(env!("CARGO_PKG_VERSION"));
 	text_hp.push_str("), ");
 	text_hp.push_str(env!("CARGO_PKG_DESCRIPTION"));
-	println!("{}\n##EXAMPLE:\n\tcargo run [--] [arg] [arg] ...", text_hp);
+	println!("{}\n", text_hp);
+	title_out();
+}
+
+fn version_out() {
+	let version = String::from(env!("CARGO_PKG_VERSION"));
+	println!("Version program my-wc: v{}", version);
 }
 
 fn title_out() {
 	// Краткое пояснение работы
-	println!("My program WC!\nEXAMPLE: cargo run [--] [arg] [arg] ...");
+	println!("EXAMPLE:\n\tcargo run [--] [param] [arg] ...");
+	println!("\nPARAMETERS:\n\t'-l'  Количество строк\n\t'-c'  Количество байт\n\t'-m'  Количество символов\n\t'-L'  Количество символов в самой длинной строке\n\t'-w'  Количество слов");
 }
 
 fn long_out(_par: &str, _file: &str) {
 	// Выполнение с доп. параметрами
 	println!("Parametr: {}, File: {}", _par, _file);
+
+	match _par {
+		"-l" => lib::param_l(_file),
+		"-c" => lib::param_c(_file),
+		"-m" => lib::param_m(_file),
+		"-L" => lib::param_bl(_file),
+		"-w" => lib::param_w(_file),
+		_ => println!("[ ERROR ] Could not verification getting parameter"),
+	}
 }
 
 fn simple_out(_file_name: &str) {
 	// Стандартное выполнение
-	if _file_name == "-h" {
+	if _file_name == "-h" || _file_name == "--help" {
 		help_out();
+	} else if _file_name == "-v" || _file_name == "--version" {
+		version_out();
 	} else {
 		println!("[ INFO ] Starting calc!");
 
-		// переменная с типом Path
-		let input_file = Path::new(_file_name); 
-
-		// переменная для открытия файла
-		let file = File::open(&input_file)
-			.expect("[ ERROR ] Failed to open file!");
+		let mod_file = lib::open_file(_file_name);
 
 		// переменная содержит полное содержимое file
-		let reader = BufReader::new(file);
+		let reader = BufReader::new(mod_file);
 
-		let mut sum = 0;
+		let mut sum_chars = 0;
+		let mut sum_lines = 0;
 
 		//
 		for line in reader.lines() {
 			let line_cont = line.unwrap();
-			sum += line_cont.len();
+
+			sum_chars += line_cont.len();
+			sum_lines += 1;
 		}
 
-		println!("Word count: {}", sum);
+		println!(" {}\tlen strings\t{}\t{}", sum_lines, sum_chars, _file_name);
 	}
 }
 
